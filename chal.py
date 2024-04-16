477ee9352281cccd73b217d48501d740acef4414
Nikola Zupancic <nikola.z37@hotmail.com>
2024-04-16 02:58:13 -0400

    added some challenge infrastructure

--- chal.py
+++ chal.py
@@ -22,19 +22,28 @@ def load_chals_json(path="challenges.json"):
 
 
 def load_challenges(path="challenges.json"):
-    chal_path = "./challenges"
+    base_chal_path = "./challenges/"
     chals = {}
-    for filename in os.listdir(chal_path):
-        chal = load_chal(filename)
-        if chal is not None:
-            chals[chal.chal_id] = chal
+    for file in os.listdir(base_chal_path):
+        # if its a directory
+        chalpath = os.path.join(base_chal_path, file)
+        if os.path.isdir(chalpath):
+            for chaldirfile in os.listdir(chalpath):
+                if chaldirfile.endswith(".yaml"):
+                    chal = load_chal(chaldirfile, base_chal_path=chalpath)
+                    if chal is not None:
+                        chals[chal.chal_id] = chal
+        else:
+            chal = load_chal(file)
+            if chal is not None:
+                chals[chal.chal_id] = chal
     return chals
 
 
-def load_chal(filename, chal_path="./challenges/"):
+def load_chal(filename=None, base_chal_path="./challenges/"):
     if not filename.endswith(".yaml"):
         return None
-    f = open(os.path.join(chal_path, filename), 'r')
+    f = open(os.path.join(base_chal_path, filename), 'r')
     chal_data = yaml.safe_load(f)
     f.close()
 
@@ -42,30 +51,49 @@ def load_chal(filename, chal_path="./challenges/"):
     chal_id = chal_data["name"]
     flag = chal_data["flag"]
     chal = Chal(chal_id, flag)
-    if "role" in chal_data:
-        chal.role_id = chal_data["role"]
-    if "attributes" in chal_data:
-        if "role" in chal_data["attributes"]:
-            chal.role_id = chal_data["attributes"]["role"]
+
     if "message" in chal_data:
         chal.description = chal_data["message"]
+
+    if "role" in chal_data:
+        chal.role_id = chal_data["role"]
+
+    if "attributes" in chal_data:
+        chal.attributes = chal_data["attributes"]
+
     if "files" in chal_data:
         if chal_data["files"] is not None:
             for chal_file in chal_data["files"]:
                 chal.files.append(chal_file)
+
     return chal
 
 
 class Chal:
+    """
+    The challenge struct
+    Attributes can be really just about anything, it will be possible to filter out challenges by different attributes, depending on what you put in the attribute field
+    For example, if you want a "difficulty and category attribute"
+    add
+    attributes:
+        difficulty: easy
+        category: steganography
+    This challenge will then appear when searching for easy or steganography
+    This is kind of like a 'hints' property (honestly could be replaced by that)
+    Just use something like hints: [easy, steganography]
+    This can then be filtered with main filter and subfilter
+    """
+
     def __init__(self, chal_id, flag,
                  description=None, role_id=None,
-                 files=[], points=0):
+                 files=[], points=0, attributes={}):
         self.chal_id = chal_id
         self.flag = flag
         self.description = description
         self.role_id = role_id
         self.files = files
         self.points = points
+        self.attributes = attributes
 
     def verify(self, flag_guess):
         return self.flag == flag_guess
